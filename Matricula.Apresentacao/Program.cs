dd97d3a8a2a92fef0fe7859a79153618c27b37f0
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2018-04-08 13:44:59 -0300

    Including Presentation layer for using project.

--- Matricula.Apresentacao/Program.cs
+++ Matricula.Apresentacao/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Matricula.Constants;
+using Matricula.Infrastructure.UoW;
 
 namespace Matricula.Apresentacao
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var stringBuilder = new StringBuilder();
+
+            while (true)
+            {
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                //Uow = new UnitOfWork("");
+
+                //IEnumerable matriculas = Uow.MatriculaRepository.ListarObj();
+                Console.WriteLine("1 - Gerar aquivo com matriculas verificadas.");
+                Console.WriteLine("2 - Gerar arquivo de matriculas com digito verificados.");
+                Console.WriteLine("O que deseja verificar?");
+                var escolha = Console.ReadLine();
+                switch (escolha)
+                {
+                    case "1":
+                        VerificarMatriculas(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), stringBuilder);
+                        break;
+                    case "2":
+                        GerarDigitoVerificador(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), stringBuilder);
+                        break;
+                    default:
+                        continue;
+                }
+
+                break;
+            }
+        }
+
+        public static void VerificarMatriculas(string basePath, StringBuilder stringBuilder)
+        {
+            stringBuilder.Append(basePath).Append(FileNames.MatriculasParaVerificar);
+            var uow = new UnitOfWork(stringBuilder.ToString());
+            stringBuilder.Clear();
+
+            IEnumerable matriculas = uow.MatriculaRepository.ListarObj();
+            var mat = new Entities.Matricula();
+            ICollection<string> resultado = new List<string>();
+
+            foreach (var matricula in matriculas)
+            {
+                if (matricula == null || matricula.Equals("")) continue;
+                mat.GerarDigitoVerificador(matricula.ToString().Substring(0, 4));
+
+                if (matricula.ToString().Substring(5, 1).Equals(mat.Dv))
+                {
+                    stringBuilder.Append(matricula).Append(" ").Append("verdadeiro").AppendLine();
+                    resultado.Add(stringBuilder.ToString());
+                }
+                else
+                {
+                    stringBuilder.Append(matricula).Append(" ").Append("falso").AppendLine();
+                    resultado.Add(stringBuilder.ToString());
+                }
+                uow.MatriculaRepository.Salvar(resultado);
+
+                stringBuilder.Clear();
+            }
+        }
+
+        public static void GerarDigitoVerificador(string basePath, StringBuilder stringBuilder)
+        {
+            stringBuilder.Append(basePath).Append(FileNames.MatriculasParaVerificar);
         }
     }
 }
